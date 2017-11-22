f1a03aab392b6112334b9c5259cba371fe5db40e
Flavio Silva <flavio.silva@iatec.com>
2017-11-22 16:37:18 -0200

    *

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,4 @@ import { MenuItemModel } from './iatec-layout/models/index';
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  public menus: Array<MenuItemModel> = [
-    <MenuItemModel>{ id: 0, title: "Todo" },
-    <MenuItemModel>{ id: 1, title: "Menu 1", parentId: 0 },
-    <MenuItemModel>{ id: 2, title: "Sub Menu 1", parentId: 1, target: "énois" },
-    <MenuItemModel>{ id: 3, title: "Menu 2", parentId: 0, target: "énoisdnv" },    
-  ]
-
-  public onClickMenu(e): void {
-    console.info(e)
-  }
-  public onClickFavorite(e): void {}
 }
