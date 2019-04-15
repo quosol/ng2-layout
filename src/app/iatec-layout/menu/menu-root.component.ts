35c80a839cb587af3b3cedd5ec21f007a13958dc
marco <marco.aguilar@iatec.com>
2019-04-15 08:23:40 -0400

    Added function scrollIntoViewIfNeeded in Element

--- src/app/iatec-layout/menu/menu-root.component.ts
+++ src/app/iatec-layout/menu/menu-root.component.ts
@@ -81,6 +81,8 @@ export class MenuRootComponent implements ControlValueAccessor, OnDestroy {
             this.monitoringRouter(ev);
         });
 
+        this.moveScroll();
+
         this.menuIndexSelected = -1;
         // Define callback for arrows event
         this.arrowsUpAndDownEvent = (ev: KeyboardEvent) => {
@@ -356,6 +358,70 @@ export class MenuRootComponent implements ControlValueAccessor, OnDestroy {
     }
 
     setDisabledState?(isDisabled: boolean): void {
-        //throw new Error("Method not implemented.");
+        // throw new Error("Method not implemented.");
+    }
+
+    private moveScroll() {
+        (Element.prototype as any).scrollIntoViewIfNeeded = function (centerIfNeeded) {
+            'use strict';
+
+            function makeRange(start, length) {
+                return { 'start': start, 'length': length, 'end': start + length };
+            }
+
+            function coverRange(inner, outer) {
+                if (
+                    false === centerIfNeeded ||
+                    (outer.start < inner.end && inner.start < outer.end)
+                ) {
+                    return Math.max(
+                        inner.end - outer.length,
+                        Math.min(outer.start, inner.start)
+                    );
+                }
+                return (inner.start + inner.end - outer.length) / 2;
+            }
+
+            function makePoint(x, y) {
+                return {
+                    'x': x,
+                    'y': y,
+                    'translate': function translate(dX, dY) {
+                        return makePoint(x + dX, y + dY);
+                    }
+                };
+            }
+
+            function absolute(elem, pt) {
+                while (elem) {
+                    pt = pt.translate(elem.offsetLeft, elem.offsetTop);
+                    elem = elem.offsetParent;
+                }
+                return pt;
+            }
+
+            let target = absolute(this, makePoint(0, 0)),
+                extent = makePoint(this.offsetWidth, this.offsetHeight),
+                elem = this.parentNode,
+                origin;
+
+            while (elem instanceof HTMLElement) {
+                // Apply desired scroll amount.
+                origin = absolute(elem, makePoint(elem.clientLeft, elem.clientTop));
+                elem.scrollLeft = coverRange(
+                    makeRange(target.x - origin.x, extent.x),
+                    makeRange(elem.scrollLeft, elem.clientWidth)
+                );
+                elem.scrollTop = coverRange(
+                    makeRange(target.y - origin.y, extent.y),
+                    makeRange(elem.scrollTop, elem.clientHeight)
+                );
+
+                // Determine actual scroll amount by reading back scroll properties.
+                target = target.translate(-elem.scrollLeft, -elem.scrollTop);
+                elem = elem.parentNode;
+            }
+        };
+
     }
 }
