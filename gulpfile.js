224fd7afa278f6fa608a421daf5053214964426d
Flávio Silva <fsdev@flaviosilva.net>
2019-06-06 09:38:34 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var inlineNg2Template = require('gulp-inline-ng2-template');
 
+require('es6-promise').polyfill();
+
 gulp.task('js:build', function () {
     return new Promise(function(resolve, reject) {
         gulp.src('./src/app/iatec-layout/**/*.ts') // also can use *.js files
